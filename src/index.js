8cd4ed27296dfc572733a002b1697d62c770fa9e
Alex <ajf@fastmail.com>
2020-05-08 17:47:33 -0500

    get job info

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ const getUserInfo = async () => {
   }
 }
 getUserInfo();
+
+const getJobInfo = async () => {
+  const apiURL = 'https://torre.co/api/opportunities/2W11EaWq';
+  try {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      console.log(data);
+  } catch (e) {
+      console.log('There was a fetch error: ' + e);
+  }
+}
+getJobInfo();
