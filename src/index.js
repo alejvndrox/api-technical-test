9ae5071364cbd8d07ef03be7c78a17e2887fe250
Alex <ajf@fastmail.com>
2020-05-08 17:42:23 -0500

    get user info

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const getUserInfo = async () => {
+  const apiURL = 'https://torre.bio/api/bios/ajf';
+  try {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      console.log(data);
+  } catch (e) {
+      console.log('There was a fetch error: ' + e);
+  }
+}
+getUserInfo();
