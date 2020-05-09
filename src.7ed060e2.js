6e85eb64ba06cfa90199ce01c968038d7b13a3f7
Alex (via Travis CI) <ajf@fastmail.com>
2020-05-09 02:07:16 +0000

    Deploy release

--- src.7ed060e2.js
+++ src.7ed060e2.js
@@ -12106,7 +12106,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58262" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58399" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
